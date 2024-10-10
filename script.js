23a45d351c59048cb4bc2334a376802bef698a18
lucabaro96 <171255333+lucabaro96@users.noreply.github.com>
2024-10-10 11:37:45 +0200

    navbar done

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // menu
-
 document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.getElementById('hamburger');
     const navLinks = document.getElementById('navLinks');
@@ -7,15 +6,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const dropdownContent = document.querySelector('.dropdown-content');
 
     hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('nav-active');
-        hamburger.innerHTML = navLinks.classList.contains('nav-active') ? '&times;' : '&#9776;'; // Cambia tra hamburger e X
+        if (navLinks.classList.contains('nav-active')) {
+            // Se il menu è attivo, attiva l'animazione di chiusura
+            navLinks.classList.add('nav-inactive'); // Aggiungi classe per animazione di chiusura
+
+            // Rimuovi la classe menu-active dopo l'animazione
+            setTimeout(() => {
+                navLinks.classList.remove('nav-active', 'nav-inactive'); // Rimuovi entrambe le classi
+            }, 500); // Imposta il timeout in base alla durata dell'animazione
+        } else {
+            // Se il menu non è attivo, attivalo
+            navLinks.classList.add('nav-active');
+        }
+        hamburger.classList.toggle('menu-active'); 
     });
 
     // Gestisci il clic su SETTORI per mostrare/nascondere il dropdown
     dropdownToggle.addEventListener('click', (e) => {
         e.preventDefault(); // Previene l'azione di default del link
-        dropdownContent.classList.toggle('dropdown-active'); // Attiva/disattiva la visibilità del dropdown
+        dropdownContent.classList.toggle('dropdown-active');
         const icon = dropdownToggle.querySelector('.dropdown-icon');
-        icon.innerHTML = dropdownContent.classList.contains('dropdown-active') ? '&#9650;' : '&#9660;'; // Cambia l'icona
+        icon.innerHTML = dropdownContent.classList.contains('dropdown-active') ? '&#9650;' : '&#9660;';
     });
 });
